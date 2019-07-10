eb093bfd6f6d664eb33ad83be28c2f175f563b3f
吴东生 <>
2019-07-10 10:40:16 +0800

    编译+测试

--- android/src/main/java/com/futurehotel/library/RNFutureModule.java
+++ android/src/main/java/com/futurehotel/library/RNFutureModule.java
@@ -1,13 +1,16 @@
 
 package com.futurehotel.library;
 
+import android.nfc.Tag;
+import android.util.Log;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
 public class RNFutureModule extends ReactContextBaseJavaModule {
-
+  public static final String TAG=RNFutureModule.class.getName();
   private final ReactApplicationContext reactContext;
 
   public RNFutureModule(ReactApplicationContext reactContext) {
@@ -19,4 +22,10 @@ public class RNFutureModule extends ReactContextBaseJavaModule {
   public String getName() {
     return "RNFuture";
   }
+
+
+  public void  textMethod(){
+    Log.e(TAG, "textMethod测试测试");
+  }
+
 }
